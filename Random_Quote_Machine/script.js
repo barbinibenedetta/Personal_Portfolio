69bb4b81b7e8d63acbdb413c54bf968c616a35c8
barbinibenedetta <barbinibenedetta@gmail.com>
2024-11-30 21:14:25 +0100

    project completed

--- Random_Quote_Machine/script.js
+++ Random_Quote_Machine/script.js
@@ -1,6 +1,34 @@
-// This project uses the Quotes API by Luke Peavey.
-// Licensed under the MIT License.
-// Copyright © 2019 Luke Peavey
+const randomQuoteURL = 'https://api.quotable.io/random';
 
-const quotesAPI = 'https://api.quotable.io';
+// DOM
 const newQuoteBtn = document.getElementById('new-quote');
+const quoteText = document.getElementById('text');
+const author = document.getElementById('author');
+const tweetBtn = document.getElementById('tweet-btn');
+const tweetLink = document.getElementById('tweet-quote');
+
+const fetchData = async () => {
+  try {
+    const res = await fetch('https://dummyjson.com/quotes/random');
+    const data = await res.json();
+    if (res.ok && data.quote.length < 160) {
+      quoteText.innerText = data.quote;
+      author.innerText = data.author;
+
+      const tweetText = `"${data.quote} - (${data.author})`
+      tweetLink.setAttribute('href', `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`);
+    } else {
+      fetchData();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fetchData();
+
+newQuoteBtn.addEventListener('click', fetchData);
+
+tweetBtn.addEventListener('click', () => {
+  
+});
