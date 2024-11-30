f50ad2fe7944abebb54150d1c9e24c941ed56a6c
barbinibenedetta <barbinibenedetta@gmail.com>
2024-11-30 14:02:06 +0100

    started project

---
+++ Random_Quote_Machine/script.js
@@ -0,0 +1,6 @@
+// This project uses the Quotes API by Luke Peavey.
+// Licensed under the MIT License.
+// Copyright © 2019 Luke Peavey
+
+const quotesAPI = 'https://api.quotable.io';
+const newQuoteBtn = document.getElementById('new-quote');
