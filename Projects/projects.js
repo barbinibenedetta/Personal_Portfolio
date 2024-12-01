e51f6bb697d824d5fff63c70ca03c9756173e4d9
barbinibenedetta <barbinibenedetta@gmail.com>
2024-12-01 13:51:09 +0100

    created file to store projects info

---
+++ Projects/projects.js
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    id: 1,
+    name: 'Tribute_Page',
+  },
+  {
+    id: 2,
+    name: 'Technical_Documentation_Page',
+  },
+  {
+    id: 3,
+    name: 'Product_Landing_Page',
+  },
+  {
+    id: 4,
+    name: 'Palindrome_Cheker',
+  },
+  {
+    id: 5,
+    name: 'Roman_Numeral_Converter',
+  },
+  {
+    id: 6,
+    name: 'Telephone_Number_Validator',
+  },
+  {
+    id: 7,
+    name: 'Pokémon_SearchApp',
+  },
+  {
+    id: 8,
+    name: 'Cash_Register',
+  },
+  {
+    id: 9,
+    name: 'Random_Quote_Machine',
+  },
+]
