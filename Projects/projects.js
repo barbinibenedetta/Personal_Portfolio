cdbe54bba35338754d0bbfa6175da5ec986f0222
barbinibenedetta <barbinibenedetta@gmail.com>
2024-12-29 14:09:00 +0100

    Added description and tags to projects

--- Projects/projects.js
+++ Projects/projects.js
@@ -2,37 +2,55 @@ export const projects = [
   {
     id: 1,
     name: 'Tribute_Page',
+    description: 'One of my first projects exploring and learning the main features of HTML and CSS.',
+    tag: ['html', 'css'],
   },
   {
     id: 2,
     name: 'Technical_Documentation_Page',
+    description: 'To build this technical documentation page regarding simple HTML features, I applied some basic CSS and implemented a lateral nagivation menu to jump to different section of the page.',
+    tag: ['html', 'css'],
   },
   {
     id: 3,
     name: 'Product_Landing_Page',
+    description: 'For this project I created a fictional brand and the landing page for their main product. I combined different CSS and HTML features.',
+    tag: ['html', 'css'],
   },
   {
     id: 4,
     name: 'Palindrome_Checker',
+    description: 'This simple program checks wheter a sentence or a word is a palindrome or not. It ignores spaces, punctuation marks and non-alphanumeric characters.',
+    tag: ['html', 'css', 'javascript'],
   },
   {
     id: 5,
     name: 'Roman_Numeral_Converter',
+    description: 'This program converts arabic numerals into the corresponding roman numerals.',
+    tag: ['html', 'css', 'javascript'],
   },
   {
     id: 6,
     name: 'Telephone_Number_Validator',
+    description: 'This program checks wheter a number is a valid US telephone number. To build this project I worked with regular expressions (regex).',
+    tag: ['html', 'css', 'javascript'],
   },
   {
     id: 7,
     name: 'Pokémon_SearchApp',
+    description: 'Using an external API containing a collection of all the existing Pokémons, i built this search application. It retrieves the desired information about the Pokémon and displays them. The card the information is displayed on changes color dinamically based on the Pokémon type.',
+    tag: ['html', 'css', 'javascript', 'api'],
   },
   {
     id: 8,
     name: 'Cash_Register',
+    description: 'This cash register project calculates the amount of change to give back to a paying customer, based on the cash provided and the cost of the item. This program makes sure that the change is given in the highest currency available and handles edge cases like and empty register and unsifficient funds. I also based this project on OOP.',
+    tag: ['html', 'css', 'javascript', 'oop'],
   },
   {
     id: 9,
     name: 'Random_Quote_Machine',
+    description: 'This projects relies on an external API containing a collection of quotes and their authors. It also involves some features from front-end libraries, mainly Bootstrap.',
+    tag: ['html', 'css', 'javascript', 'api', 'bootstrap'],
   },
 ]
